80dc2ed8b48f5138452906c46ff77088fa4ddc1f
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-04-25 17:59:57 +0530

    ys

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,27 +156,27 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#       'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'fcpi',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345678',
-#         'HOST': 'localhost',
-#         'PORT': '5432',     
-#     }
-#   }
-
 DATABASES = {
       'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hoztoxin_fcpi',
-        'USER': 'hoztoxin_fcpiuser',
-        'PASSWORD': 'Fcpi@963',
-        'HOST': 'https://hoztox.in',
+        'NAME': 'fcpi',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
         'PORT': '5432',     
     }
   }
+
+# DATABASES = {
+#       'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hoztoxin_fcpi',
+#         'USER': 'hoztoxin_fcpiuser',
+#         'PASSWORD': 'Fcpi@963',
+#         'HOST': 'https://hoztox.in',
+#         'PORT': '5432',     
+#     }
+#   }
 
 # DATABASES = {
 #        "default": {
